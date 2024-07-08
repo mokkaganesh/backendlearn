e8ec4fdc8cff66c1288739cc6b1a71dfcfbd2051
Ganesh <2110080050@klh.edu.in>
2024-07-08 23:19:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// import express from 'express'
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/names', (req, res) => {
+    res.send('Hello Names!')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Login Page</h1>')
+})
+
+app.get('/names/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}!`)
+    //how to pass the name in localhost
+    //localhost:3000/names/John
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
